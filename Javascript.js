c5696e4f31bee155a419a5bf46503f5e83474615
javck009 <84823936+javck009@users.noreply.github.com>
2021-06-06 08:35:35 +0530

    Codersboyz

---
+++ Javascript.js
@@ -0,0 +1,16 @@
+
+function oppen(){
+  document.getElementById('slide-bar').style.width="250px";
+  
+   
+}
+function closenav(){
+  document.getElementById('slide-bar').style.width="0";
+  
+}
+
+let loader=document.getElementById('loader');
+function bitcoin() {
+  // body...
+  loader.style.display="none";
+}
